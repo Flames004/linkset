33daf1d3ce7aaf68eec1ea7ca1eda76237ea39e6
Deepak Shukla <flamesofwisdom004@gmail.com>
2025-07-23 12:15:15 +0530

    install dependencies and modules

---
+++ app/_layout.tsx
@@ -0,0 +1,12 @@
+// app/_layout.tsx
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function Layout() {
+  return (
+    <>
+      <StatusBar style="dark" />
+      <Stack screenOptions={{ headerShown: false }} />
+    </>
+  );
+}
